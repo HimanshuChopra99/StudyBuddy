a1bca9281aa9a44358d88f0eab72bffbc44c9ff4
Himanshu Chopra <choprahimanshu99@gmail.com>
2025-08-09 10:56:34 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://study-buddy-ecru-two.vercel.app"],
+    origin: "*",
     credentials: true,
   })
 );
